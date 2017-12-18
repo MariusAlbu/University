fc949805e651b2bf9fbcceb50fb41a073b013cc9
Marius Albu <hallowed_dragon@yahoo.com>
2017-12-18 14:54:44 -0800

    UI Base

---
+++ MA.University/MA.University.UI/Scripts/Example2/eventsController.js
@@ -0,0 +1,14 @@
+﻿var EventsController = function () {
+    this.PopulateList = function (events) {
+        var jqEventsUL = $("#eventsList"); //jquery
+        for (var i = 0; i < events.length; i++) {
+            jqEventsUL.append("<li class='myILClass' id='" + events[i].id + "'>" + events[i].name + "</li>");
+        }
+        $("#eventsContainer").append(jqEventsUL);
+        lowLetter();
+    }
+
+    function lowLetter() {
+
+    }
+}
---
+++ MA.University/MA.University.UI/Scripts/Example2/eventsService.js
@@ -0,0 +1,24 @@
+﻿var EventsService = function () {
+    var _events = [{
+        "id": 1,
+        "name": "Team Building"
+    },
+    {
+        "id": 2,
+        "name": "Workshop"
+    }
+        ,
+    {
+        "id": 3,
+        "name": "Birthday"
+    }
+    ];
+
+    this.ReadAll = function () {
+        return _events;
+    }
+
+    this.Insert = function (event) {
+        _event.Add(event);
+    }
+}
